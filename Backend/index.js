dd372691dba2e02da4de81a1839fa8e0a35ebf11
Nikhita1504 <136711092+Nikhita1504@users.noreply.github.com>
2024-11-22 19:41:02 +0530

    skills-filter-done

--- Backend/index.js
+++ Backend/index.js
@@ -2,47 +2,37 @@ const express = require("express");
 const app = express();
 const bodyparser = require("body-parser");
 const cors = require("cors");
-const { authrouter } = require("./Router/Authrouter");
-const { google_auth_router } = require("./Router/Google_Authcontroller");
+const {authrouter} = require("./Router/Authrouter")
+const {google_auth_router} = require("./Router/Google_Authcontroller")
 const Profilerouter = require("./Router/Profilerouter");
+const { UserModel } = require("./model/db");
 const homerouter = require("./Router/homerouter");
+const editprofilerouter = require("./Router/editprofilerouter");
 const hackathondetails = require("./Router/hackathondetails");
 const SearchMembers = require("./Router/SearchMembers");
 const TeamDetails = require("./Router/TeamDetails");
-const CheckEnrollRouter = require("./Router/CheckEnrollRouter");
+const CheckEnrollRouter = require("./Router/CheckEnrollRouter")
 require("dotenv").config();
-require("./model/db");
-
-const { createServer } = require("http");
-const { Server } = require("socket.io");
-
-const server = createServer(app); 
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
-
-
-
+require("./model/db")
 
 const Port = process.env.Port || 3000;
 
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded());
+app.use(bodyparser.urlencoded())
 app.use(cors());
+app.use("/auth" , authrouter)
+app.use("/google/auth",google_auth_router)
+app.use("/profile" , Profilerouter)
+app.use("/home",homerouter)
+app.use("/hackathon",hackathondetails)
+app.use("/add-member",SearchMembers)
+app.use("/team",TeamDetails)
+app.use("/enroll",CheckEnrollRouter)
 
-// Routes
-app.use("/auth", authrouter);
-app.use("/google/auth", google_auth_router);
-app.use("/profile", Profilerouter);
-app.use("/home", homerouter);
-app.use("/hackathon", hackathondetails);
-app.use("/add-member", SearchMembers);
-app.use("/team", TeamDetails); // Attach TeamDetails to main app
-app.use("/enroll", CheckEnrollRouter);
 
-server.listen(Port, () => {
-  console.log("Server is running on port", Port);
-});
+// app.use("/home/edit-profile",editprofilerouter)
+
+
+app.listen(Port, () =>{
+  console.log("server is Running")
+})
